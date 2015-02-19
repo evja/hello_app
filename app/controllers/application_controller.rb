b6129a8940665b7eaa52929b4b2aebc95a788452
Evan A <evjay.a@gmail.com>
2015-02-18 22:12:33 -0700

    addeda new controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hola Mundo"
+    render text: "¡Hola Mundo"
+  end
+
+  def goodbye
+    render text: "See ya later aligator"
   end
 end
